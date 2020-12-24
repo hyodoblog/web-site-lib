719c96baa64df09da389a47d131734b1da123fce
yusei-hyodo <yusei.hyodo@gmail.com>
2020-12-24 14:41:19 +0900

    eslint prettierをアップデート

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,6 @@
 export default {
   srcDir: 'src/',
   ssr: false,
-  /*
-   ** Headers of the page
-   */
   head: {
     titleTemplate: "%s",
     title: process.env.npm_package_name || "",
@@ -20,25 +17,10 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  /*
-   ** Customize the progress-bar color
-   */
   loading: { color: "#fff" },
-  /*
-   ** Global CSS
-   */
   css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
   plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: ["@nuxtjs/vuetify"],
-  /*
-   ** Nuxt.js modules
-   */
+  buildModules: ['@nuxt/typescript-build', "@nuxtjs/vuetify"],
   modules: ["nuxt-fontawesome"],
   fontawesome: {
     imports: [
@@ -52,43 +34,5 @@ export default {
       },
     ],
   },
-  /*
-   ** vuetify module configuration
-   ** https://github.com/nuxt-community/vuetify-module
-   */
-  // vuetify: {
-  //   customVariables: ['~/assets/variables.scss'],
-  //   theme: {
-  //     dark: true,
-  //     themes: {
-  //       dark: {
-  //         primary: colors.blue.darken2,
-  //         accent: colors.grey.darken3,
-  //         secondary: colors.amber.darken3,
-  //         info: colors.teal.lighten1,
-  //         warning: colors.amber.base,
-  //         error: colors.deepOrange.accent4,
-  //         success: colors.green.accent3
-  //       }
-  //     }
-  //   }
-  // },
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/,
-        })
-      }
-    },
-  },
+  build: {},
 }
